6228e858063abc54bc58d6cf0c9d8c8bf7f84b0e
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2021-03-03 13:21:48 -0300

    commit class members
    update: aprendendo sobre os membros de uma classe

--- app/src/main/java/com/example/digionebank/Pessoa.kt
+++ app/src/main/java/com/example/digionebank/Pessoa.kt
@@ -2,19 +2,21 @@ package com.example.digionebank
 
 class Pessoa {
     var nome: String = "João Pedro"
-
     var cpf: String = "123.456.789-00"
     private set
 
     inner class Endereco {
         var rua: String = "Rua teste"
     }
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val joaopedro = Pessoa()
 
-    println(joaopedro.nome)
-    println(joaopedro.cpf)
-    println(joaopedro.Endereco().rua)
+    println(joaopedro)
+    println(joaopedro.pessoaInfo())
 }
